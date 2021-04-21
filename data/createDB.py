22454ec31b99a7858fd7840eae50b7671eb764d6
motestw64631 <44443471+motestw64631@users.noreply.github.com>
2021-04-21 22:26:07 +0800

    complete 2 apis includes attraction and attractions

---
+++ data/createDB.py
@@ -0,0 +1,52 @@
+import json
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, create_engine,Integer,ForeignKey,Float
+from sqlalchemy.orm import sessionmaker,relationship,backref
+
+
+Base = declarative_base()
+
+engine = create_engine('mysql+mysqlconnector://root:motestw64631@localhost:3306/travelweb')
+
+
+class TravelSpot(Base):
+    # 表的名字
+    __tablename__ = 'travel_spot'
+    # 表的結構
+    id = Column(Integer, primary_key=True,autoincrement=False)
+    name = Column(String(255),nullable=False)
+    transport = Column(String(8000),nullable=True)
+    category = Column(String(1000),nullable=False)
+    longitude = Column(Float,nullable=False)
+    latitude = Column(Float,nullable=False)
+    address = Column(String(1000),nullable=False)
+    describe = Column(String(9000),nullable=False)
+    mrt = Column(String(1000),nullable=True)
+    db_url = relationship('Url',backref='travelspot')
+    def __init__(self,id,name,transport,category,longitude,latitude,address,describe,mrt):
+        self.id = id
+        self.name = name
+        self.transport = transport
+        self.category = category
+        self.longitude = longitude
+        self.latitude = latitude
+        self.address = address
+        self.describe = describe
+        self.mrt = mrt
+    def __repr__(self):
+        return "<Spot('%s')>" % (self.name)
+
+class Url(Base):
+    __tablename__ = 'url'
+    id = Column(Integer, primary_key=True,autoincrement=True)
+    spot_id = Column(Integer, ForeignKey('travel_spot.id'))
+    url = Column(String(8000),nullable=False)
+    db_travel_spot = relationship('TravelSpot',backref='url')
+    def __init__(self,spot_id,url):
+        self.spot_id = spot_id
+        self.url = url
+    def __repr__(self):
+        return f"<url {self.spot_id}>"
+
+#Base.metadata.create_all(engine)
---
+++ data/utils.py
@@ -0,0 +1,49 @@
+from .createDB import Base,engine,TravelSpot,Url
+from sqlalchemy.orm import sessionmaker
+
+Session = sessionmaker(bind=engine)
+session=Session()
+
+'''
+for i in session.query(TravelSpot).filter_by(id=1):
+    print(i.name)
+'''
+
+def data_to_json(data):
+    return {
+        'id':data.id,
+        'name':data.name,
+        'category':data.category,
+        "description": data.describe,
+        "address": data.address,
+        "transport": data.transport,
+        "mrt": data.mrt,
+        "latitude": data.latitude,
+        "longitude": data.longitude,
+        "img":[i.url for i in data.url],
+    }
+
+def find_attraction_by_id(id):
+    attraction = session.query(TravelSpot).filter_by(id=id).first()
+    if not attraction:
+        return None
+    return data_to_json(attraction)
+
+def find_attraction_all(page):
+    data_count = session.query(TravelSpot).count()
+    page_count = data_count//12 
+    next_page = page+1 if page<page_count else None
+    data = session.query(TravelSpot).slice(12*page,12*(page+1)).all()
+    data_json = [data_to_json(i) for i in data]
+    return {'nextPage':next_page,'data':data_json}
+
+
+def find_attraction_by_name(name,page):
+    data_count =  session.query(TravelSpot).filter(TravelSpot.name.like(f'%{name}%')).count()
+    page_count = data_count//12
+    next_page = page+1 if page<page_count else None
+    data = session.query(TravelSpot).filter(TravelSpot.name.like(f'%{name}%')).slice(12*page,12*(page+1)).all()
+    data_json = [data_to_json(i) for i in data]
+    return {'nextPage':next_page,'data':data_json}
+
+#print(find_attraction_all(0))
